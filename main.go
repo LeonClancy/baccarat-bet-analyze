76f4d282f7d5dd3cd1ed7845a6a274a53b091186
Leon <astu9702@gmail.com>
2024-02-18 23:01:33 +0800

    fix: cors config

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"github.com/LeonClancy/baccarat-bet-analyze/controller"
 	"github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/filter/cors"
 )
 
 func main() {
+	web.InsertFilter("*", web.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	rc := &controller.RoadmapController{}
 	web.Get("/roadmap", rc.GetRoadmap)
 	web.Post("/roadmap", rc.NewRoadmap)
